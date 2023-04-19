34f921c4b641d052a4c292ee40c6f18694cd7de1
wsoksoda <75330996+wsoksoda@users.noreply.github.com>
2023-04-19 11:32:12 -0500

    renamed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,12 @@ import { HomeComponent } from './home/home.component';
 import { BookOrderComponent } from './book-order/book-order.component';
 import { EventsComponent } from './events/events.component';
 import { AdminComponent } from './admin/admin.component';
-import { TitleComponent } from './title/title.component';
-import { ImageSlideshowComponent } from './image-slideshow/image-slideshow.component';
-import { InfoComponent } from './info/info.component';
 import { ContactInfoComponent } from './contact-info/contact-info.component';
-import { BoardOfDirectorsComponent } from './board-of-directors/board-of-directors.component';
+import { HomeImageSlideshowComponent } from './home-image-slideshow/home-image-slideshow.component';
+import { HomeBoardOfDirectorsComponent } from './home-board-of-directors/home-board-of-directors.component';
+import { HomeInfoComponent } from './home-info/home-info.component';
+import { HomeTitleComponent } from './home-title/home-title.component';
+
 
 @NgModule({
   declarations: [
@@ -22,11 +23,11 @@ import { BoardOfDirectorsComponent } from './board-of-directors/board-of-directo
     BookOrderComponent,
     EventsComponent,
     AdminComponent,
-    TitleComponent,
-    ImageSlideshowComponent,
-    InfoComponent,
     ContactInfoComponent,
-    BoardOfDirectorsComponent,
+    HomeImageSlideshowComponent,
+    HomeBoardOfDirectorsComponent,
+    HomeInfoComponent,
+    HomeTitleComponent
   ],
   imports: [
     BrowserModule,
