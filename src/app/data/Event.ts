f9bec3ff8c8627a15f0b52b75f068615c261de8e
BowlBird <millercarson111@gmail.com>
2023-04-26 18:22:11 -0500

    added event type

---
+++ src/app/data/Event.ts
@@ -0,0 +1,9 @@
+export interface Event {
+	id: number,
+	title: string,
+	description: string,
+	address: string,
+	isLiteracyClubAttending: boolean,
+	startDate: Date
+	endDate: Date
+}
--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent {
-
+  
 }
