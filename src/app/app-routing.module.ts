b2efcb5bddbddeca6e922a85167dc351477ffb00
BowlBird <millercarson111@gmail.com>
2023-04-26 16:30:25 -0500

    Implemented Routing for Home, Events, Admin, and Orders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EventsComponent } from './events/events.component';
+import { AdminComponent } from './admin/admin.component';
+import { BookOrderComponent } from './book-order/book-order.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'events', component: EventsComponent},
+  { path: 'admin', component: AdminComponent},
+  { path: 'order', component: BookOrderComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent { }
